d7c0c8d65d973c81fcdbbee630a11a3123b819f6
iamthanuj <thanujdevil@gmail.com>
2024-01-03 23:15:20 +0530

    registation func added

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -12,8 +12,6 @@ const initialState = {
   message: "",
 };
 
-
-
 //Register User
 export const register = createAsyncThunk(
   "auth/register",
@@ -32,9 +30,6 @@ export const register = createAsyncThunk(
   }
 );
 
-
-
-
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -46,7 +41,23 @@ export const authSlice = createSlice({
       state.message = "";
     },
   },
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
 });
 
 export const { reset } = authSlice.actions;
