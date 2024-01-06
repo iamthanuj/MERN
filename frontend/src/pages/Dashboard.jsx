dadb0d6b132835b769b1d5f88c69dd7321fd40fd
iamthanuj <thanujdevil@gmail.com>
2024-01-06 23:27:31 +0530

    page redirection added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import { useEffect } from 'react'
 
-function dashboard() {
+
+function Dashboard() {
+
+  const navigate = useNavigate()
+  const {user}  = useSelector((state)=>state.auth)
+
+  useEffect(()=>{
+    if(!user){
+      navigate('/login')
+    }
+  }, [user, navigate])
+
   return (
     <div>dashboard</div>
   )
 }
 
-export default dashboard
+export default Dashboard
