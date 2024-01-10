10cd468c7500dfbf383fceb1535c166d7fe0513c
iamthanuj <thanujdevil@gmail.com>
2024-01-10 23:56:09 +0530

    get gaols action fixed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,28 +1,51 @@
-import {useNavigate} from 'react-router-dom'
-import {useSelector} from 'react-redux'
-import { useEffect } from 'react'
-import GoalForm from '../components/GoalForm'
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const navigate = useNavigate()
-  const {user}  = useSelector((state)=>state.auth)
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
-  useEffect(()=>{
-    if(!user){
-      navigate('/login')
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
     }
-  }, [user, navigate])
 
+    if (!user) {
+      navigate("/login");
+    }
+
+    dispatch(getGoals());
+
+  }, [user, navigate, isError, message, dispatch]);
+
+
+  if(isLoading){
+    return <Spinner/>
+  }
   return (
     <>
-      <section className='heading'>
+      <section className="heading">
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
-      <GoalForm/>
+
+      <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (goals.map((goal)=>(<GoalItem goal={goal} key={goal._id} />))) : (<h3>No Goals</h3>) }
+      </section>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
